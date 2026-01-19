7fc7050cbe028f5acdbd83605102a2efc2644a4f
Imanhak1m <152905677+Imanhak1m@users.noreply.github.com>
2026-01-19 21:07:33 +0800

    Add Firebase SDK initialization and configuration

---
+++ js/firebase.js
@@ -0,0 +1,26 @@
+// Firebase SDK (Web - module version)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+import { 
+  getAuth, 
+  signInWithEmailAndPassword,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
+
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/12.8.0/firebase-firestore.js";
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDehIoo1ViD1DV9zh1phJMSQOiAHFmbl9I",
+  authDomain: "project-reporting-system-54a9b.firebaseapp.com",
+  projectId: "project-reporting-system-54a9b",
+  storageBucket: "project-reporting-system-54a9b.firebasestorage.app",
+  messagingSenderId: "688470828500",
+  appId: "1:688470828500:web:77b0d3431f1e5a1419a241"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
