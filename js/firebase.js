a2ecfee2e95a1965fddbe7f2e5afbb0e0c0cbc38
Imanhak1m <152905677+Imanhak1m@users.noreply.github.com>
2026-01-20 14:17:14 +0800

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,16 +1,20 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-firestore.js";
+// js/firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDehIoo1ViD1DV9zh1phJMSQOiAHFmbl9I",
   authDomain: "project-reporting-system-54a9b.firebaseapp.com",
   projectId: "project-reporting-system-54a9b",
-  storageBucket: "project-reporting-system-54a9b.firebasestorage.app",
+  storageBucket: "project-reporting-system-54a9b.appspot.com",
   messagingSenderId: "688470828500",
   appId: "1:688470828500:web:77b0d3431f1e5a1419a241"
 };
 
-export const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// EXPORT THESE
 export const auth = getAuth(app);
 export const db = getFirestore(app);
