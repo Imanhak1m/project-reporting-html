1a1f813fbc1c0a6e02d965b81c15b33d54121e4c
Imanhak1m <152905677+Imanhak1m@users.noreply.github.com>
2026-01-20 09:01:36 +0800

    Implement authentication state handling and logout

--- js/dashboard-auth.js
+++ js/dashboard-auth.js
@@ -1,1 +1,20 @@
+import { auth } from "./firebase.js";
+import { signOut, onAuthStateChanged } from
+  "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
+
+const welcomeText = document.getElementById("welcomeText");
+const logoutBtn = document.getElementById("logoutBtn");
+
+onAuthStateChanged(auth, (user) => {
+  if (!user) {
+    window.location.href = "index.html";
+  } else {
+    welcomeText.textContent = "Welcome Admin";
+  }
+});
+
+logoutBtn.addEventListener("click", async () => {
+  await signOut(auth);
+  window.location.href = "index.html";
+});
 
